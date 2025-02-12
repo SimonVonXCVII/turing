13ac97e26b1f3d3afeee7d9b77596ac04d5dd73b
Simon Von <g1672943850@gmail.com>
2025-02-12 05:33:01 -0500

    🔨 Dependency Upgrades 1. Upgrade to Elastic 8.15.5 2. Upgrade to Kotlin 2.1.10 3. Upgrade to Redis 7.4.2 4. Upgrade to Nginx 1.27.4 5. Upgrade to Gradle 8.2.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,14 +9,14 @@ plugins {
     /**
      * 添加对具有给定 id 的插件的依赖关系。
      */
-    id("org.springframework.boot") version "3.4.1"
+    id("org.springframework.boot") version "3.4.2"
     id("io.spring.dependency-management") version "1.1.7"
     id("org.graalvm.buildtools.native") version "0.10.4"
     /**
      * 应用给定的 Kotlin 插件模块。
      */
-    kotlin("jvm") version "2.1.0"
-    kotlin("plugin.spring") version "2.1.0"
+    kotlin("jvm") version "2.1.10"
+    kotlin("plugin.spring") version "2.1.10"
 }
 
 /**
