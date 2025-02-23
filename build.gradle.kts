caa14f50b3ab299e6a13ad044777ad57d075e5d4
Simon Von <g1672943850@gmail.com>
2025-02-22 22:37:03 -0500

    🔨 Dependency Upgrades 1. Upgrade to Spring Boot 3.4.3 2. Upgrade to Portainer 2.27.0 3. Upgrade to Amazoncorretto 23.0.2-al2023

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     /**
      * 添加对具有给定 id 的插件的依赖关系。
      */
-    id("org.springframework.boot") version "3.4.2"
+    id("org.springframework.boot") version "3.4.3"
     id("io.spring.dependency-management") version "1.1.7"
     id("org.graalvm.buildtools.native") version "0.10.4"
     /**
