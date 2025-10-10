98635feac3ad577ffc28f4a18546b91c63566d8d
Simon Von <g1672943850@gmail.com>
2025-10-10 19:03:35 +0800

    ⭐ New Features 1. 给 docker 容器 keycloak 分配专属数据库 keycloak
    Signed-off-by: Simon Von <g1672943850@gmail.com>

--- src/main/java/com/simonvonxcvii/turing/TuringApplication.kt
+++ src/main/java/com/simonvonxcvii/turing/TuringApplication.kt
@@ -17,6 +17,7 @@ import org.springframework.data.web.config.EnableSpringDataWebSupport
 // todo 尝试将 dto 改成 spring-data-jpa 一样的 record 类。IXxProjection 投影类。@org.hibernate.envers.Audited 类
 // todo 朝着 oidc 发展，或许也是朝着多模块甚至是为服务方向发展
 // todo 是否需要 Role（角色表）添加前缀“ROLE_”
+// todo docker 安装了那些容器，能否启动它们的图形化界面？比如数据库？
 @EnableConfigurationProperties(CustomSecurityProperties::class)
 // 清晰化 jpa 包路径
 @EnableJpaRepositories("com.simonvonxcvii.turing.repository.jpa")
