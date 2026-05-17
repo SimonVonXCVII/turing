bb961329c86ecfd51db6ee7d3e3e47373e519cf7
Simon Von <g1672943850@gmail.com>
2026-05-17 12:31:37 -0400

    📔 Documentation 1. 優化根 build.gradle.kts
    Signed-off-by: Simon Von <g1672943850@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,16 +18,11 @@ allprojects {
     version = "0.0.1-SNAPSHOT"
 }
 
+val javaLanguage = libs.versions.java.language.get().toInt()
+val springBootAdmin = libs.de.codecentric.spring.boot.admin.dependencies.get().toString()
+
 subprojects {
-    /**
-     * 应用零个或多个插件或脚本。
-     * 给定的闭包用于配置 ObjectConfigurationAction，它“构建”插件应用程序。
-     * 此方法与 apply(Map) 的不同之处在于，它允许多次调用配置操作的方法。
-     */
     apply {
-        /**
-         * 添加一个插件用于配置目标对象。 您可以多次调用此方法，以使用多个插件。 脚本和插件按照添加顺序应用。
-         */
         plugin("java")
         plugin("java-library")
         plugin("idea")
@@ -40,53 +35,24 @@ subprojects {
 //        plugin("checkstyle")
     }
 
-    /**
-     * 配置 org.gradle.api.plugins.JavaPluginExtension 扩展。
-     */
     java {
-        /**
-         * 配置需要工具链中的工具的任务的项目范围工具链要求（例如 org.gradle.api.tasks.compile.JavaCompile）。
-         * 配置工具链不能与此扩展上的 sourceCompatibility 或 targetCompatibility 一起使用。这两个值都将来自工具链。
-         */
         toolchain {
-            /**
-             * 工具链需要支持的 Java 语言的确切版本。
-             */
-            languageVersion = JavaLanguageVersion.of(26)
-//            languageVersion = JavaLanguageVersion.of(libs.versions.java.language.get()) todo
+            languageVersion = JavaLanguageVersion.of(javaLanguage)
         }
     }
 
-    /**
-     * 配置 org.gradle.api.file.SourceDirectorySet 扩展。
-     */
     kotlin {
-        /**
-         * 配置项目中 Kotlin JVM 和 Java 任务的 Java 工具链。
-         */
         jvmToolchain {
-            languageVersion = JavaLanguageVersion.of(26)
+            languageVersion = JavaLanguageVersion.of(javaLanguage)
         }
     }
 
-    /**
-     * 返回该项目的配置。
-     */
     configurations {
-        /**
-         * 提供现有的 compileOnly 元素。
-         */
         compileOnly {
-            /**
-             * 将给定配置添加到此配置所扩展的配置集中。
-             */
             extendsFrom(configurations.annotationProcessor.get())
         }
     }
 
-    /**
-     * 配置 org.gradle.api.plugins.quality.CheckstyleExtension 扩展。
-     */
 //    checkstyle {
 //        toolVersion = "9.3"
 //    }
@@ -97,9 +63,6 @@ subprojects {
 //    }
 //}
 
-    /**
-     * 配置该项目的子项目都有的依赖
-     */
     dependencies {
 //        implementation("org.springframework.boot:spring-boot-starter")
 //        testImplementation("org.springframework.boot:spring-boot-starter-test")
@@ -107,13 +70,7 @@ subprojects {
 //        checkstyle("io.spring.javaformat:spring-javaformat-checkstyle")
     }
 
-    /**
-     * spring 以外依赖的统一管理
-     */
     dependencyManagement {
-        /**
-         * 使用给定操作配置托管依赖项。
-         */
 //        dependencies { todo
 //            /**
 //             * 为给定 id 标识的依赖项配置依赖项管理。 id 是一个格式为 group:name:version 的字符串。
@@ -127,8 +84,7 @@ subprojects {
 //        }
 
         imports {
-            mavenBom("de.codecentric:spring-boot-admin-dependencies:4.0.4")
-//            mavenBom(libs.de.codecentric.spring.boot.admin.dependencies.get().toString())
+            mavenBom(springBootAdmin)
         }
     }
 
